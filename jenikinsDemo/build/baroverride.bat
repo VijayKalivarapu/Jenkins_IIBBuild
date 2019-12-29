9cb7ceef72248a96c30546237bbacce7640282f1
vijay,kumar <vikaliva@in.ibm.com>
2019-12-29 16:17:30 +0530

    updated the Baroverride bat file

--- jenikinsDemo/build/baroverride.bat
+++ jenikinsDemo/build/baroverride.bat
@@ -1,3 +1,5 @@
 @echo off
-CALL C:/Program Files/IBM/IIB/10.0.0.10/server/bin/mqsiprofile.cmd
+iib.home = C:/Program Files/IBM/IIB/10.0.0.10
+iib.mqsi.path = ${iib.home}/server/bin
+CALL ${iib.mqsi.path}/mqsiprofile.cmd
 CALL mqsiapplybaroverride -b %1 -p %2 -r
